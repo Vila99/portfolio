5e77511a2c15740c29d6ff8011f4cd287b22611e
Vila <guillemvilapamies@gmail.com>
2024-09-04 00:09:39 +0200

    Nav bar + header

---
+++ app/components/ContentPage.js
@@ -0,0 +1,50 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+const ContentComponent = () => {
+  return (
+    <section>
+      <div id="sobre-mi" className="container section-container">
+        <h1 className="fw-light section-title">SOBRE MÍ.</h1>
+        <p className="lead pt-4">
+          Después de 5 años cursando ADE en la Universitat de Barcelona, decidí
+          ampliar mis conocimientos en programación y edición de contenidos.
+          Esta actividad es mucho más coherente respecto a mis intereses y
+          hobbies. Además, las oportunidades laborales vinculadas al sector
+          multimedia resultan mucho más atractivas, por proyecto y valorización de
+          competencias. Actualmente, me encuentro desarrollando y aprendiendo
+          nuevas habilidades para trabajar en estos nuevos campos.
+        </p>
+      </div>
+
+      <div id="proyectos" className="container section-container">
+        <h1 className="fw-light section-title">PROYECTOS.</h1>
+        <p className="lead pt-4">
+          Después de 5 años cursando ADE en la Universitat de Barcelona, decidí
+          ampliar mis conocimientos en programación y edición de contenidos.
+          Esta actividad es mucho más coherente respecto a mis intereses y
+          hobbies. Además, las oportunidades laborales vinculadas al sector
+          multimedia resultan mucho más atractivas, por proyecto y valorización de
+          competencias. Actualmente, me encuentro desarrollando y aprendiendo
+          nuevas habilidades para trabajar en estos nuevos campos.
+        </p>
+      </div>
+
+      <div id="contacto" className="container section-container">
+        <h1 className="fw-light section-title">CONTACTO.</h1>
+        <p className="lead pt-4 mb-0 pb-5">
+          Después de 5 años cursando ADE en la Universitat de Barcelona, decidí
+          ampliar mis conocimientos en programación y edición de contenidos.
+          Esta actividad es mucho más coherente respecto a mis intereses y
+          hobbies. Además, las oportunidades laborales vinculadas al sector
+          multimedia resultan mucho más atractivas, por proyecto y valorización de
+          competencias. Actualmente, me encuentro desarrollando y aprendiendo
+          nuevas habilidades para trabajar en estos nuevos campos.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default ContentComponent;
---
+++ app/components/MainPage.js
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Typewriter from 'typewriter-effect';
+
+const MainComponent = () => {
+  const [isClient, setIsClient] = useState(false);
+
+
+  // Se monta despues del cliente
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <header className="masthead bg-main">
+      <div className="container h-100">
+        <div className="row h-100 align-items-center">
+          <div className="col-12 text-center">
+            <h1 className="titulo-main">
+              {isClient && (
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString('¡Hello!')
+                      .pauseFor(1000)
+                      .deleteAll()
+                      .typeString("I'm Guillem Vila.")
+                      .start();
+                  }}
+                />
+              )}
+            </h1>
+            <p className="lead fs-4">
+              Frontend Developer.
+            </p>
+            <p className="lead fs-4">
+              "Always learning and improving to be able to create better web designs."
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default MainComponent;
